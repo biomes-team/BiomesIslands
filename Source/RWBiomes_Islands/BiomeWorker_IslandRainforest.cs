8522e56881484d59c6398890fa5b9829d0570f95
zyllle <zyll3on@gmail.com>
2019-12-06 19:12:43 -0700

    Tropical rainforest and swamp islands

---
+++ Source/RWBiomes_Islands/BiomeWorker_IslandRainforest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using RimWorld.Planet;
+using Verse;
+
+namespace RWBiomes_Islands
+{
+    // Never spawns the biome
+    public class BiomeWorker_IslandRainforest : BiomeWorker
+    {
+        public override float GetScore(Tile tile, int tileID)
+        {
+
+            if (!tile.WaterCovered)
+            {
+                return -100f;
+            }
+            if (tile.elevation < -300f || tile.elevation > -120f)
+            {
+                return 0f;
+            }
+            if (Rand.Value < 0.992f)
+            {
+                return 0f;
+            }
+
+
+            if (tile.temperature < 15f)
+            {
+                return 0f;
+            }
+            if (tile.rainfall < 2000f)
+            {
+                return 0f;
+            }
+            return 28f + (tile.temperature - 20f) * 1.5f + (tile.rainfall - 600f) / 165f;
+            
+        }
+    }
+}
---
+++ Source/RWBiomes_Islands/BiomeWorker_Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using RimWorld.Planet;
+
+namespace RWBiomes_Islands
+{
+    // Spawns the biome on every tile
+    public class BiomeWorker_Test : BiomeWorker
+    {
+        public override float GetScore(Tile tile, int tileID)
+        {
+
+            return 100;
+
+        }
+    }
+}
