7488b2a2dd54570175a3cad1d4a1b80046854626
zyllle <zyll3on@gmail.com>
2020-10-10 17:36:17 -0600

    Adjustments to seaweed, algae, plankton

--- Source/BiomesIslands/BiomesIslands/Incidents/Incident_GlowTide.cs
+++ Source/BiomesIslands/BiomesIslands/Incidents/Incident_GlowTide.cs
@@ -25,9 +25,10 @@ namespace BiomesIslands.Incidents
 
     class GameCondition_GlowTide : GameCondition
     {
-        private float movementDir = 0f;
+        // direction the tide is approaching from
+        private Rot4 tideDir;
 
-        private float pctLeft
+        private float pctRemaining
         {
             get
             {
@@ -38,9 +39,7 @@ namespace BiomesIslands.Incidents
         public override void Init()
         {
             base.Init();
-
-            movementDir = Rand.Range(0, 360);
-            // find out what else to init
+            tideDir = Rot4.Random;
         }
 
         public override void GameConditionTick()
@@ -55,12 +54,39 @@ namespace BiomesIslands.Incidents
                     Map map = affectedMaps[i];
                     int mapSize = map.Size.x;
                     int tideSize = mapSize / 10;
-
-                    int spawnCount = Rand.Range(1, 15);
+                    int spawnCount = 7;
 
                     for(int j = 0; j < spawnCount; j++)
                     {
-                        IntVec3 spawnSpot = CellFinderLoose.RandomCellWith((IntVec3 c) => c.GetFirstBuilding(map) == null && PlantUtility.CanEverPlantAt_NewTemp(BiomesIslandsDefOf.BiomesIslands_GlowPlankton, c, map) && c.x < tideSize + pctLeft * mapSize && c.x > -tideSize + pctLeft * mapSize, map, 500);
+                        IntVec3 spawnSpot;
+                        if(tideDir == Rot4.East)
+                        {
+                            spawnSpot = CellFinderLoose.RandomCellWith((IntVec3 c) => c.GetFirstBuilding(map) == null
+                                        && PlantUtility.CanEverPlantAt_NewTemp(BiomesIslandsDefOf.BiomesIslands_GlowPlankton, c, map)
+                                        && c.x < tideSize + pctRemaining * mapSize
+                                        && c.x > -tideSize + pctRemaining * mapSize, map, 500);
+                        }
+                        else if (tideDir == Rot4.West)
+                        {
+                            spawnSpot = CellFinderLoose.RandomCellWith((IntVec3 c) => c.GetFirstBuilding(map) == null
+                                        && PlantUtility.CanEverPlantAt_NewTemp(BiomesIslandsDefOf.BiomesIslands_GlowPlankton, c, map)
+                                        && c.x < tideSize + (1 - pctRemaining) * mapSize
+                                        && c.x > -tideSize + (1 - pctRemaining) * mapSize, map, 500);
+                        }
+                        else if (tideDir == Rot4.South)
+                        {
+                            spawnSpot = CellFinderLoose.RandomCellWith((IntVec3 c) => c.GetFirstBuilding(map) == null
+                                        && PlantUtility.CanEverPlantAt_NewTemp(BiomesIslandsDefOf.BiomesIslands_GlowPlankton, c, map)
+                                        && c.z < tideSize + (1 - pctRemaining) * mapSize
+                                        && c.z > -tideSize + (1 - pctRemaining) * mapSize, map, 500);
+                        }
+                        else
+                        {
+                            spawnSpot = CellFinderLoose.RandomCellWith((IntVec3 c) => c.GetFirstBuilding(map) == null
+                                        && PlantUtility.CanEverPlantAt_NewTemp(BiomesIslandsDefOf.BiomesIslands_GlowPlankton, c, map)
+                                        && c.z < tideSize + pctRemaining * mapSize
+                                        && c.z > -tideSize + pctRemaining * mapSize, map, 500);
+                        }
 
                         if (spawnSpot != null)
                         {
